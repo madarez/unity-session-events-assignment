34890be016b06d3ab5fc82e98559948d18bc8ade
Rez GodarzvandChegini <RGodarzvandChegini@globeandmail.com>
2020-10-25 15:45:37 -0400

    add missing comma

--- test_integration.py
+++ test_integration.py
@@ -36,7 +36,7 @@ class SessionEventsServiceIntegrationTest(unittest.TestCase):
         return [
             {'player_id': 'cff7a233780b43ca8b256260b6759f0d', 'country': 'CA', 'event': 'start', 'session_id': '2ab87ad3-4542-4a8e-b9b9-40c609ffdad5', 'ts': '2016-11-28T20:50:22'},
             {'player_id': 'cff7a233780b43ca8b256260b6759f0d', 'event': 'end', 'session_id': '2ab87ad3-4542-4a8e-b9b9-40c609ffdad5', 'ts': '2016-11-29T04:18:25'},
-            {'player_id': '7569b1ec9a934730bfe0891d88c47041', 'country': 'US', 'event': 'start', 'session_id': 'a702cf5c-d837-4017-9b63-2ee836faa76d', 'ts': '2016-11-15T18:48:02'}
+            {'player_id': '7569b1ec9a934730bfe0891d88c47041', 'country': 'US', 'event': 'start', 'session_id': 'a702cf5c-d837-4017-9b63-2ee836faa76d', 'ts': '2016-11-15T18:48:02'},
             {'player_id': 'cff7a233780b43ca8b256260b6759f0d', 'country': 'CA', 'event': 'start', 'session_id': 'b7f09fe5-bd3b-4370-a576-5ae73e9a7851', 'ts': '2016-11-24T20:50:22'},
             {'player_id': 'cff7a233780b43ca8b256260b6759f0d', 'event': 'end', 'session_id': 'b7f09fe5-bd3b-4370-a576-5ae73e9a7851', 'ts': '2016-11-25T04:18:25'},
             {'player_id': 'cff7a233780b43ca8b256260b6759f0d', 'country': 'CA', 'event': 'start', 'session_id': '70620aea-1bff-4924-9d3a-06cfefb3ac1b', 'ts': '2016-11-29T04:49:19'},
